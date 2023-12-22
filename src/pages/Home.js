fb866a9fa6e7d7da6b9dea0fcb57ec98616954fa
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-12-22 16:37:16 +0500

    getdocs replace with onsnapshot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import {
+  getDocs,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from 'firebase/firestore';
 import { auth, db } from '../firebase-config';
 import { useNavigate } from 'react-router-dom';
 
 function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
+  const [initialLoad, setInitialLoad] = useState(false);
   const postsCollectionRef = collection(db, 'posts');
   const navigate = useNavigate();
 
@@ -12,49 +19,57 @@ function Home({ isAuth }) {
     const postDoc = doc(db, 'posts', id);
     await deleteDoc(postDoc);
   };
-  useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
 
-    getPosts();
-  }, [deletePost]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(postsCollectionRef, (snapshot) => {
+      setPostList(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setInitialLoad(true); // Mark initial load as complete
+    });
+
+    return () => unsubscribe(); // Cleanup function to prevent memory leaks
+  }, []); // Empty dependency array to run only once
+
+  // useEffect(() => {
+  //   const getPosts = async () => {
+  //     const data = await getDocs(postsCollectionRef);
+  //     setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
+
+  //   getPosts();
+  // }, [deletePost]);
 
   return (
     <div className='homePage'>
-      {postLists.map((post) => {
-        return (
-          <div
-            className='post'
-            key={post.id}
-            onClick={() => navigate(`/posts/${post.id}`)}
-          >
-            <div className='postHeader'>
-              <div className='title'>
-                <h1 className='text-3xl'> {post.title}</h1>
-              </div>
-              <div className='deletePost'>
-                {isAuth && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    &#128465;
-                  </button>
-                )}
-              </div>
+      {postLists.map((post) => (
+        <div
+          className='post'
+          key={post.id}
+          onClick={() => navigate(`/posts/${post.id}`)}
+        >
+          <div className='postHeader'>
+            <div className='title'>
+              <h1 className='text-3xl'> {post.title}</h1>
             </div>
-            <div className='postTextContainer'>
-              <div> Gender: {post.gender} </div>
-              <div> Age : {post.age} </div>
-              <div> {post.postText} </div>
+            <div className='deletePost'>
+              {isAuth && post.author.id === auth.currentUser.uid && (
+                <button
+                  onClick={() => {
+                    deletePost(post.id);
+                  }}
+                >
+                  &#128465;
+                </button>
+              )}
             </div>
-            <h3>@{post.author.name}</h3>
           </div>
-        );
-      })}
+          <div className='postTextContainer'>
+            <div> Gender: {post.gender} </div>
+            <div> Age : {post.age} </div>
+            <div> {post.postText} </div>
+          </div>
+          <h3>@{post.author.name}</h3>
+        </div>
+      ))}
     </div>
   );
 }
